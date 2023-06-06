f752270bd029a1ed4fcca562e229c28c49517198
Vansh bhardwaj <89251548+Vansh-77@users.noreply.github.com>
2023-06-06 19:07:17 +0530

    Update homepage.dart
    ..

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -38,7 +38,7 @@ class HomePage extends StatelessWidget {
                     child: Column(
                       children: [
                         Text(
-                          "23'C",
+                          "24'C",
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             color: Colors.white,
