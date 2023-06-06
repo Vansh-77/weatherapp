b0d52b2c3528e1e66268205c33316322bed7f0ff
Vansh-77 <vanshbhardwaj7306@gmail.com>
2023-06-06 19:04:56 +0530

    added listview.builder

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -10,82 +10,111 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(34, 42, 55, 1),
       body: SingleChildScrollView(
-        child: Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: SizedBox(
-                        height: maxheight * 0.55,
-                        width: maxwidth,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset(
-                            "assets/images/clear.jpg",
-                            fit: BoxFit.fill,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: SizedBox(
+                      height: maxheight * 0.55,
+                      width: maxwidth,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Image.asset(
+                          "assets/images/rainy.jpg",
+                          fit: BoxFit.fill,
+                        ),
+                      )),
+                ),
+                Container(
+                  alignment: Alignment.bottomRight,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                      right: maxwidth * 0.1,
+                      top: maxheight * 0.35,
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          "23'C",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                blurRadius: 20,
+                                offset: Offset.fromDirection(0, 2),
+                              )
+                            ],
+                            fontSize: 50,
                           ),
-                        )),
-                  ),
-                  Container(
-                    alignment: Alignment.bottomRight,
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: maxwidth * 0.1,
-                        vertical: maxheight * 0.35,
-                      ),
-                      child: Column(
-                        children: [
-                          Text(
-                            "32'C",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              shadows: [
-                                Shadow(
-                                  color: Colors.black,
-                                  blurRadius: 20,
-                                  offset: Offset.fromDirection(0, 2),
-                                )
-                              ],
-                              fontSize: 50,
-                            ),
+                        ),
+                        Text(
+                          "rainy",
+                          style: TextStyle(
+                            fontSize: 23,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                color: Colors.black,
+                                blurRadius: 10,
+                                offset: Offset.fromDirection(0, 2),
+                              )
+                            ],
                           ),
-                          Text(
-                            "clear",
-                            style: TextStyle(
-                              fontSize: 23,
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              shadows: [
-                                Shadow(
-                                  color: Colors.black,
-                                  blurRadius: 10,
-                                  offset: Offset.fromDirection(0, 2),
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
+            for (int i = 0; i < 10; i++)
               Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(12.0),
                 child: Container(
+                  padding: const EdgeInsets.all(10),
                   width: maxwidth,
-                  height: maxheight,
+                  height: maxheight * 0.2,
                   decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.amber),
+                    borderRadius: BorderRadius.circular(20),
+                    color: const Color.fromRGBO(0, 10, 40, 1),
+                  ),
+                  child: ListView.separated(
+                    padding: const EdgeInsets.all(0),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    separatorBuilder: (context, index) {
+                      return const SizedBox(
+                        width: 10,
+                      );
+                    },
+                    itemBuilder: (context, index) {
+                      return Container(
+                        width: maxwidth * 0.25,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color.fromARGB(255, 28, 93, 125),
+                        ),
+                        child: Center(
+                          child: Text(
+                            (index + 1).toString(),
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
-            ],
-          ),
+          ],
         ),
       ),
     );
